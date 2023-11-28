e552d432ac403260c8092d40d80bc0036c8f019f
s3xyoda <ynyd0601@gmail.com>
2023-11-28 20:02:23 +0800

    Initial commit

---
+++ contracts/CARVToken.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+/// @custom:security-contact security@carv.io
+contract CARVToken is ERC20, ERC20Burnable, Ownable, ERC20Permit, ERC20Votes {
+    constructor(address initialOwner)
+        ERC20("CARVToken", "ARC")
+        Ownable(initialOwner)
+        ERC20Permit("CARVToken")
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
